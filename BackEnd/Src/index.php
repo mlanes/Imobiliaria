16f53d7d9ce761cf824ece4e3549d0285f20cb79
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 01:09:33 -0200

    Adicionando exibição de erros no projeto

--- BackEnd/Src/index.php
+++ BackEnd/Src/index.php
@@ -7,6 +7,13 @@
 // }
 // spl_autoload_register("autoloadClasses");
 
+ini_set( 'display_errors', TRUE );
+error_reporting( E_ALL | E_STRICT );
+
+
+// Pegar url que fez o request
+// var_dump($_SERVER["HTTP_REFERER"]);
+
 require_once "../Config/App.php";
 require_once ("Core/Mvc.php");
 
