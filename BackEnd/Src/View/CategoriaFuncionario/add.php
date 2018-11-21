046b7d1b4f78a514b72e1dbba773ab2c997036c8
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 02:36:28 -0200

    Adicionando exibição nos campos dos valores que foram previamente digitados mas não passaram por validação

--- BackEnd/Src/View/CategoriaFuncionario/add.php
+++ BackEnd/Src/View/CategoriaFuncionario/add.php
@@ -1,6 +1,6 @@
 <form method="post">
-    <input name="nm_categoria">
-    <input name="ic_status">
-    <input name="nm_sigla">
+    <input name="nm_categoria" value="<?= $nm_categoria ?>">
+    <input name="ic_status" value="<?= $ic_status ?>">
+    <input name="nm_sigla" value="<?= $nm_sigla ?>">
     <button>Salvar</button>
 </form>
