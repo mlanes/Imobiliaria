9a5513068203c9dbf6961b8714a69ab0605f8c9c
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 01:10:21 -0200

    Adicionando método que redireciona a página de volta a requisitante

--- BackEnd/Src/Core/Controller.php
+++ BackEnd/Src/Core/Controller.php
@@ -2,16 +2,37 @@
 
 class Controller
 {
+    protected $reffer;
 
-    public function loadModel($modelName) {
+    public function __construct()
+    {
+    }
+
+    private function getReffer()
+    {
+        $this->reffer = $_SERVER["HTTP_REFERER"];
+    }
+
+    public function redirectUrl(String $url = null)
+    {
+        if ($url != null) {
+            header("Location: " . $url);
+        } else {
+            $this->getReffer();
+            header("Location: " . $this->reffer);
+        }
+    }
+
+    public function loadModel($modelName)
+    {
         $modelName .= 'Model';
         require_once "Model/" . $modelName . ".php";
         return new $modelName();
     }
 
-    public function loadView($folderName, $viewName) {
+    public function loadView($folderName, $viewName)
+    {
         $file = "View/" . ucfirst($folderName) . "/" . $viewName . '.php';
         return $file;
     }
-
 }
