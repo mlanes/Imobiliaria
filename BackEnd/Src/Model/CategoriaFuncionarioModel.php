a540b813fbb913cc346266c5acbb89180938f0db
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 01:14:03 -0200

    Adicionando métodos de habilitar, desabilitar e implementando a interface de crud.

--- BackEnd/Src/Model/CategoriaFuncionarioModel.php
+++ BackEnd/Src/Model/CategoriaFuncionarioModel.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once 'Core/Model.php';
+require_once 'Interfaces/CrudInterface.php';
 
-class CategoriaFuncionarioModel extends Model
+class CategoriaFuncionarioModel extends Model implements CrudInterface
 {
-
     private $cd_categoria;
     private $nm_categoria;
     private $ic_status;
     private $nm_sigla;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -24,8 +25,9 @@ class CategoriaFuncionarioModel extends Model
         return $this->$name;
     }
 
-    public function countTotal() {
-        try{
+    public function countItems()
+    {
+        try {
             $sql = "SELECT COUNT(*) as COUNT FROM tb_categoria_funcionario;";
             $stmt = $this->link->prepare($sql);
             $stmt->execute();
@@ -41,8 +43,9 @@ class CategoriaFuncionarioModel extends Model
         }
     }
 
-    public function insert() {
-        try{
+    public function insert()
+    {
+        try {
             $sql = "INSERT INTO user(name) VALUES(:name);";
             $stmt = $this->link->prepare($sql);
             $stmt->bindValue(":name", "mateus");
@@ -52,8 +55,9 @@ class CategoriaFuncionarioModel extends Model
         }
     }
 
-    public function select() {
-        try{
+    public function select()
+    {
+        try {
             $sql = "SELECT * FROM tb_categoria_funcionario;";
             $stmt = $this->link->prepare($sql);
             $stmt->execute();
@@ -64,4 +68,29 @@ class CategoriaFuncionarioModel extends Model
         }
     }
 
+    public function disable()
+    {
+        try {
+            $sql = "UPDATE tb_categoria_funcionario SET ic_status = :ic_status WHERE cd_categoria = :cd_categoria;";
+            $stmt = $this->link->prepare($sql);
+            $stmt->bindValue(":ic_status", 0);
+            $stmt->bindValue(":cd_categoria", $this->cd_categoria);
+            $stmt->execute();
+        } catch (Exception $e) {
+            echo '<p>Erro: <b>' . $e->getMessage() . '</b></p>';
+        }
+    }
+
+    public function enable()
+    {
+        try {
+            $sql = "UPDATE tb_categoria_funcionario SET ic_status = :ic_status WHERE cd_categoria = :cd_categoria;";
+            $stmt = $this->link->prepare($sql);
+            $stmt->bindValue(":ic_status", 1);
+            $stmt->bindValue(":cd_categoria", $this->cd_categoria);
+            $stmt->execute();
+        } catch (Exception $e) {
+            echo '<p>Erro: <b>' . $e->getMessage() . '</b></p>';
+        }
+    }
 }
