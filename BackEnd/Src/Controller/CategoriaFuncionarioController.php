0c4fede64cc0b99e3755f273df7c3d85fb1a9bf1
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 01:13:04 -0200

    Adicionando rotas e lógicas para habilitar e desabilitar categorias de funcionário

--- BackEnd/Src/Controller/CategoriaFuncionarioController.php
+++ BackEnd/Src/Controller/CategoriaFuncionarioController.php
@@ -4,24 +4,51 @@ require_once 'Core/Controller.php';
 
 class CategoriaFuncionarioController extends Controller
 {
-
+    private $controller = 'CategoriaFuncionario';
     private $view;
 
-    public function __construct() {
+    public function __construct()
+    {
+        parent::__construct();
         $this->CategoriaFuncionario = parent::loadModel("CategoriaFuncionario");
     }
 
-    public function index($param) {
-
+    public function index($param)
+    {
         $categoriasFuncionario = $this->CategoriaFuncionario->select();
-        $count = $this->CategoriaFuncionario->countTotal();
+        $count = $this->CategoriaFuncionario->countItems();
 
         require_once parent::loadView('CategoriaFuncionario', $this->view);
-
     }
 
-    public function setView($a) {
+    public function setView($a)
+    {
         $this->view = $a;
     }
 
+    public function disable(array $param)
+    {
+        $param1 = $param[0];
+        if ($param1 != "") {
+            $this->CategoriaFuncionario->cd_categoria = $param1;
+            $categoriasFuncionario = $this->CategoriaFuncionario->disable();
+            // header("Location: " . $this->reffer);
+            $this->redirectUrl();
+        } else {
+            echo 'É necessário um código';
+        }
+    }
+
+    public function enable(array $param)
+    {
+        $param1 = $param[0];
+        if ($param1 != "") {
+            $this->CategoriaFuncionario->cd_categoria = $param1;
+            $categoriasFuncionario = $this->CategoriaFuncionario->enable();
+            header("Location: " . $this->reffer);
+            $this->redirectUrl();
+        } else {
+            echo 'É necessário um código';
+        }
+    }
 }
