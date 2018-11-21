a54f876f8d00d5f7401e81a649cc8a538c8dc33f
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-21 01:12:00 -0200

    Removendo espaços desnecessários

--- BackEnd/Src/Controller/DefaultController.php
+++ BackEnd/Src/Controller/DefaultController.php
@@ -4,15 +4,13 @@ require_once 'Core/Controller.php';
 
 class DefaultController extends Controller
 {
-
-    public function __construct() {
-        
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    public function notfound($folderName, $fileName) {
-
+    public function notfound($folderName, $fileName)
+    {
         require_once parent::loadView($folderName, $fileName);
-
     }
-
 }
--- BackEnd/Src/Controller/UserController.php
+++ BackEnd/Src/Controller/UserController.php
@@ -4,15 +4,16 @@ require_once 'Core/Controller.php';
 
 class UserController extends Controller
 {
-
     private $view;
 
-    public function __construct() {
+    public function __construct()
+    {
+        parent::__construct();
         $this->User = parent::loadModel("User");
     }
 
-    public function index($param) {
-
+    public function index($param)
+    {
         $user = "Mateus";
         // echo 'a: ' . $this->view;
         // echo $param;
@@ -20,11 +21,10 @@ class UserController extends Controller
         $users = $this->User->select();
 
         require_once parent::loadView('User', $this->view);
-
     }
 
-    public function setView($a) {
+    public function setView($a)
+    {
         $this->view = $a;
     }
-
 }
