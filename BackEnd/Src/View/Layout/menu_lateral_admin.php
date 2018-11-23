8b69eadb3eb6cd22df03b05794924c24e139bf0a
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-23 17:37:39 -0200

    Adicionando link para controller de Acesso

--- BackEnd/Src/View/Layout/menu_lateral_admin.php
+++ BackEnd/Src/View/Layout/menu_lateral_admin.php
@@ -6,6 +6,9 @@
         <li>
             <a href="<?= HOME_URL . 'Funcionario' ?>">Funcionarios</a>
         </li>
+        <li>
+            <a href="<?= HOME_URL . 'Acesso' ?>">Acessos</a>
+        </li>
     </ul>
 </nav>
 <hr>
