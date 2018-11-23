17f98dcff52ca84592b03e589713ff714ee494ca
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-23 16:34:16 -0200

    Adicionando menu apenas para ajudar no desenvolvimento

---
+++ BackEnd/Src/View/Layout/menu_lateral_admin.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li>
+            <a href="<?= HOME_URL . 'CategoriaFuncionario' ?>">Categorias de Funcionario</a>
+        </li>
+        <li>
+            <a href="<?= HOME_URL . 'Funcionario' ?>">Funcionarios</a>
+        </li>
+    </ul>
+</nav>
+<hr>
