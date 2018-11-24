a97c5d60f4399d348097480efdbd8c885fed7e87
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-24 01:17:18 -0200

    Adicionando campos faltantes

--- BackEnd/Src/View/Funcionario/view.php
+++ BackEnd/Src/View/Funcionario/view.php
@@ -7,8 +7,18 @@
 <p><?= $funcionario->ic_status ?></p>
 <b>Nome Completo</b>
 <p><?= $funcionario->nm_primeiro . ' ' . $funcionario->nm_meio . ' ' . $funcionario->nm_ultimo ?></p>
+<b>Data de Nascimento</b>
+<p><?= $funcionario->dt_nascimento ?></p>
 <b>CPF</b>
 <p><?= $funcionario->cd_cpf ?></p>
 <b>Categoria</b>
 <p><?= $nm_categoria ?></p>
+<?php
+    if ($funcionario->cd_creci != "") {
+        ?>
+        <b>Creci</b>
+        <p><?= $funcionario->cd_creci ?></p>
+        <?php
+    }
+?>
 <a href="<?= HOME_URL . $this->controller ?>">Voltar</a>
