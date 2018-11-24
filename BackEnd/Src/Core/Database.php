1ac8a6cc6d71b36025504204e8116a8045c8bb9c
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-24 01:21:44 -0200

    Removendo comentários desnecessários

--- BackEnd/Src/Core/Database.php
+++ BackEnd/Src/Core/Database.php
@@ -2,21 +2,20 @@
 
 class Database
 {
-
     private $db_host = HOSTNAME;
     private $db_name = DB_NAME;
     private $db_user = DB_USER;
     private $db_password = DB_PASSWORD;
 
     protected static $link;
-    // protected $link;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->link = $this->connect();
-        // $this->link = 'aaaa';
     }
 
-    public function connect() {
+    public function connect()
+    {
         try {
             $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
             $link = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password, $options);
@@ -25,5 +24,4 @@ class Database
             echo '<p>Erro: <b>' . $e->getMessage() . '</b></p>';
         }
     }
-
 }
--- BackEnd/Src/Core/Model.php
+++ BackEnd/Src/Core/Model.php
@@ -4,14 +4,11 @@ require_once 'Core/Database.php';
 
 class Model extends Database
 {
-
     private $table;
     protected $lastId;
 
     public function __construct()
     {
-        // echo "Conexão com o Banco.";
         parent::__construct();
     }
-
 }
