5c3a9195c70906f131bacbf89e606556ab4731e5
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-24 02:04:59 -0200

    Adicionando lógica de edição de funcionário e view de edição

--- BackEnd/Src/Controller/FuncionarioController.php
+++ BackEnd/Src/Controller/FuncionarioController.php
@@ -73,6 +73,67 @@ class FuncionarioController extends Controller
 
     public function edit(array $param)
     {
+        $cd_funcionario = $param[0];
+        if ($cd_funcionario != "") {
+            $nm_primeiro = isset($_POST['nm_primeiro']) ? $_POST['nm_primeiro'] : null;
+            $nm_meio = isset($_POST['nm_meio']) ? $_POST['nm_meio'] : null;
+            $nm_ultimo = isset($_POST['nm_ultimo']) ? $_POST['nm_ultimo'] : null;
+            $dt_nascimento = isset($_POST['dt_nascimento']) ? $_POST['dt_nascimento'] : null;
+            $cd_cpf = isset($_POST['cd_cpf']) ? $_POST['cd_cpf'] : null;
+
+            $ic_status = isset($_POST['ic_status']) ? $_POST['ic_status'] : null;
+            $cd_categoria = isset($_POST['cd_categoria']) ? $_POST['cd_categoria'] : null;
+            $cd_creci = isset($_POST['cd_creci']) ? $_POST['cd_creci'] : null;
+
+            $this->Pessoa = parent::loadModel("Pessoa");
+
+            if ($nm_primeiro != null && $nm_meio != null && $nm_ultimo != null && $dt_nascimento != null
+            && $cd_cpf != null && $ic_status != null && $cd_categoria != null) {
+                $this->Funcionario->cd_funcionario = $cd_funcionario;
+                $this->Funcionario->ic_status = $ic_status;
+                $this->Funcionario->cd_categoria = $cd_categoria;
+                $this->Funcionario->cd_creci = $cd_creci;
+                $funcionario = $this->Funcionario->select();
+                $cd_pessoa = $funcionario->cd_pessoa;
+                $this->Funcionario->cd_pessoa = $cd_pessoa;
+                $this->Funcionario->update();
+
+                $this->Pessoa->cd_pessoa = $cd_pessoa;
+                $this->Pessoa->nm_primeiro = $nm_primeiro;
+                $this->Pessoa->nm_meio = $nm_meio;
+                $this->Pessoa->nm_ultimo = $nm_ultimo;
+                $this->Pessoa->dt_nascimento = $dt_nascimento;
+                $this->Pessoa->dt_criado = $funcionario->dt_criado;
+                $this->Pessoa->dt_editado = date("Y-m-d H:i:s");
+                $this->Pessoa->cd_cpf = $cd_cpf;
+
+                $this->Pessoa->update();
+                $this->redirectUrl($this->controller);
+                exit;
+            } else {
+                $this->Funcionario->cd_funcionario = $cd_funcionario;
+                $funcionario = $this->Funcionario->select();
+                $ic_status = $funcionario->ic_status;
+                $cd_categoria = $funcionario->cd_categoria;
+                $cd_creci = $funcionario->cd_creci;
+                $cd_pessoa = $funcionario->cd_pessoa;
+                $nm_primeiro = $funcionario->nm_primeiro;
+                $nm_meio = $funcionario->nm_meio;
+                $nm_ultimo = $funcionario->nm_ultimo;
+                $dt_nascimento = $funcionario->dt_nascimento;
+                $dt_criado = $funcionario->dt_criado;
+                $dt_editado = $funcionario->dt_editado;
+                $cd_cpf = $funcionario->cd_cpf;
+                $this->CategoriaFuncionario = parent::loadModel("CategoriaFuncionario");
+                $categorias = $this->CategoriaFuncionario->list();
+            }
+        } else {
+            echo 'É necessário um código';
+            $this->redirectUrl();
+            exit;
+        }
+
+        require_once parent::loadView($this->controller, $this->view);
     }
 
     public function view(array $param)
---
+++ BackEnd/Src/View/Funcionario/edit.php
@@ -0,0 +1,37 @@
+<?php
+    require_once parent::loadView('Layout', 'menu_lateral_admin');
+?>
+<h1>Editar Funcionário</h1>
+<form method="post">
+    <label>Primeiro Nome</label><br>
+    <input name="nm_primeiro" value="<?= $nm_primeiro ?>"><br>
+    <label>Nome do meio</label><br>
+    <input name="nm_meio" value="<?= $nm_meio ?>"><br>
+    <label>Último Nome</label><br>
+    <input name="nm_ultimo" value="<?= $nm_ultimo ?>"><br>
+    <label>Data de Nascimento</label><br>
+    <input type="date" name="dt_nascimento" value="<?= $dt_nascimento ?>"><br>
+    <label>CPF</label><br>
+    <input name="cd_cpf" value="<?= $cd_cpf ?>"><br>
+    <label>Status</label><br>
+    <input name="ic_status" value="<?= $ic_status ?>"><br>
+    <label>Categoria</label><br>
+    <select name="cd_categoria">
+        <?php
+            foreach ($categorias as $categoria) {
+                $op = '<option value=' . $categoria->cd_categoria;
+                if ($cd_categoria == $categoria->cd_categoria) {
+                    $op .= ' selected>';
+                }
+                else {
+                    $op .= '>';
+                }
+                echo $op . $categoria->nm_categoria . '</option>';
+            }
+        ?>
+    </select><br>
+    <label>Creci</label><br>
+    <input name="cd_creci" value="<?= $cd_creci ?>"><br>
+    <button>Salvar</button>
+</form>
+<a href="<?= HOME_URL . $this->controller ?>">Voltar</a>
