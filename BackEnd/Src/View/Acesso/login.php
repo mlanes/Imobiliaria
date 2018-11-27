ca5c217f2104000d5ae32c956454b6ab53f419fd
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-27 12:33:39 -0200

    Reformulando a view de login para facilitar os testes

--- BackEnd/Src/View/Acesso/login.php
+++ BackEnd/Src/View/Acesso/login.php
@@ -1,5 +1,7 @@
 <form method="POST">
-    <input type="text" name="login" value="<?= $login ?>">
-    <input type="password" name="password">
+    <label>Login</label><br>
+    <input type="text" name="login" value="<?= $login ?>"><br>
+    <label>Password</label><br>
+    <input type="password" name="password"><br>
     <button>Testar</button>
 </form>
