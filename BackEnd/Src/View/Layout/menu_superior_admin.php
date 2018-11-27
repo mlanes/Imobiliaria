a4da7f3e33da148521d3b140adeb29a3fc648839
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-27 00:06:38 -0200

    Adicionando menu superior e modificando o lateral para o novo estilo visual

---
+++ BackEnd/Src/View/Layout/menu_superior_admin.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand mr-auto" href="#">Imobiliária</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="#">Perfil</a>
+            </li> -->
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sair</a>
+            </li>
+        </ul>
+    </div>
+</nav>
