290102dd340537ff94de4c9ac68b61f99c24b1d7
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-27 03:52:15 -0200

    Adicionando view para dashboard

---
+++ BackEnd/Src/View/Default/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?= $bootstrapHelper->css() ?>
+        <?= $styleHelper->css() ?>
+    </head>
+    <body>
+        <?php
+            require_once parent::loadView('Layout', 'menu_superior_admin');
+        ?>
+        <div id="wrapper">
+            <?php require_once parent::loadView('Layout', 'menu_lateral_admin'); ?>
+            <div id="page-content-wrapper">
+                <div class="container-fluid xyz">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>Dashboard</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
