5836dcc087c24a67afa00efd51c71da3e63b3add
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-30 14:51:14 -0200

    Arrumando arquivo de configuração

--- BackEnd/Config/App.default.php
+++ BackEnd/Config/App.default.php
@@ -1,15 +1,19 @@
 <?php
 
+// Url Base
 define('HOME_URL', '127.0.0.1/');
-define('CONTROLLERS_PATH', 'Controller/');
-define('CORE_PATH', 'Core/');
+define('SYSTEM_NAME', 'Sistema');
 
 // Database
-define('HOSTNAME', 'localhost');
+define('HOSTNAME', '');
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
 define('DB_CHARSET', 'utf-8');
 
+// Região
 define('DATE_TIMEZONE', 'America/Sao_Paulo');
+
+// Exibição
+define('SYSTEM_NAME', 'Imobiliária Litoral');
 define('DISPLAY_ERRORS', true);
