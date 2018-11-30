87aec9471d64bc488b74cb0e9ae2821914f6c983
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-11-30 15:20:18 -0200

    Adicionando nome dinâmico ao menu

--- BackEnd/Src/View/Layout/menu_superior_admin.php
+++ BackEnd/Src/View/Layout/menu_superior_admin.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
     <!-- Brand -->
-    <a class="navbar-brand mr-auto" href="<?= HOME_URL ?>">Imobiliária</a>
+    <a class="navbar-brand mr-auto" href="<?= HOME_URL ?>"><?= SYSTEM_NAME ?></a>
 
     <!-- Toggler/collapsibe Button -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
