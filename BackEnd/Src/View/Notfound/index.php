c8a4fff46c48666b827f4c85961648f3fffcbab0
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-12-01 16:33:02 -0200

    Criando view para not found

---
+++ BackEnd/Src/View/Notfound/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="<?= SYSTEM_LANG ?>">
+    <head>
+        <?php
+            require_once parent::loadView('Layout', 'head_admin');
+        ?>
+    </head>
+    <body>
+        <div>
+            <svg class="image404"></svg>
+            <a href="<?= HOME_URL ?>" class="btn btn-block btn-primary">Voltar</a>
+        </div>
+    </body>
+</html
