7f4f594043604fcd4df53ed9628a204b1eaacf54
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-12-01 13:50:57 -0200

    Adicionando regras de redirecionamento caso o usuário esteja deslogado

--- BackEnd/Src/Controller/CategoriaFuncionarioController.php
+++ BackEnd/Src/Controller/CategoriaFuncionarioController.php
@@ -3,6 +3,7 @@
 use Core\Controller;
 use Validate\CategoriaFuncionario\Add as Add;
 use Validate\Sanitize;
+use Auth\Auth;
 
 class CategoriaFuncionarioController extends Controller
 {
@@ -17,60 +18,39 @@ class CategoriaFuncionarioController extends Controller
 
     public function index()
     {
-        // Pegando dados
-        $categoriasFuncionario = $this->CategoriaFuncionario->list();
+        // Instanciando a classe
+        $auth = new Auth();
         
-        // Pegando contagem de total de itens
-        $count = $this->CategoriaFuncionario->countItems();
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            // Pegando dados
+            $categoriasFuncionario = $this->CategoriaFuncionario->list();
+            
+            // Pegando contagem de total de itens
+            $count = $this->CategoriaFuncionario->countItems();
 
-        // Caregando Helpers
-        $bootstrapHelper = parent::loadHelper("Bootstrap");
-        $styleHelper = parent::loadHelper("Style");
-        $linkHelper = parent::loadHelper("Link");
+            // Caregando Helpers
+            $bootstrapHelper = parent::loadHelper("Bootstrap");
+            $styleHelper = parent::loadHelper("Style");
+            $linkHelper = parent::loadHelper("Link");
 
-        // Carregando View
-        require_once parent::loadView($this->controller, $this->currentAction);
+            // Carregando View
+            require_once parent::loadView($this->controller, $this->currentAction);
+            exit;
+        }
+
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
+        exit;
     }
 
     public function add()
     {
-        // Chamando Validação
-        $catValidate = new Add();
-        $catValidate->validate();
+        // Instanciando a classe
+        $auth = new Auth();
 
-        // Pegando Dados da requisição de forma dinâmica e automática
-        $sanitized = new Sanitize();
-        $data = $sanitized->sanitized();
-        
-        // Verificando erro de Validação
-        if (!$catValidate->hasErrors()) {
-            $this->CategoriaFuncionario->nm_categoria = $data->nm_categoria;
-            $this->CategoriaFuncionario->setIcStatus($data->ic_status);
-            $this->CategoriaFuncionario->nm_sigla = $data->nm_sigla;
-            $this->CategoriaFuncionario->insert();
-
-            // Redirecionando para a action index
-            $this->redirectUrl($this->controller);
-            exit;
-        }
-
-        // Caregando Helpers
-        $bootstrapHelper = parent::loadHelper("Bootstrap");
-        $styleHelper = parent::loadHelper("Style");
-        $linkHelper = parent::loadHelper("Link");
-        $formHelper = parent::loadHelper("Form");
-
-        // Carregando View
-        require_once parent::loadView($this->controller, $this->currentAction);
-    }
-
-    public function edit(array $param)
-    {
-        // Pegando valor do parâmetro
-        $cd_categoria = $param[0];
-
-        // Verificando se o código está preechido
-        if (!empty($cd_categoria)) {
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
             // Chamando Validação
             $catValidate = new Add();
             $catValidate->validate();
@@ -78,27 +58,13 @@ class CategoriaFuncionarioController extends Controller
             // Pegando Dados da requisição de forma dinâmica e automática
             $sanitized = new Sanitize();
             $data = $sanitized->sanitized();
-
-            // Definindo Código
-            $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
-
-            // Buscando Dados
-            $categoriaFuncionario = $this->CategoriaFuncionario->select();
-
-            // Formatando Status para formulário
-            if ($categoriaFuncionario->ic_status == 1) {
-                $ic_status = 'enable';
-            } else {
-                $ic_status = 'disable';
-            }
-
+            
             // Verificando erro de Validação
             if (!$catValidate->hasErrors()) {
-                $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
                 $this->CategoriaFuncionario->nm_categoria = $data->nm_categoria;
                 $this->CategoriaFuncionario->setIcStatus($data->ic_status);
                 $this->CategoriaFuncionario->nm_sigla = $data->nm_sigla;
-                $this->CategoriaFuncionario->update();
+                $this->CategoriaFuncionario->insert();
 
                 // Redirecionando para a action index
                 $this->redirectUrl($this->controller);
@@ -116,78 +82,172 @@ class CategoriaFuncionarioController extends Controller
             exit;
         }
 
-        echo 'É necessário um código';
-        $this->redirectUrl();
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
+        exit;
+    }
+
+    public function edit(array $param)
+    {
+        // Instanciando a classe
+        $auth = new Auth();
+
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            // Pegando valor do parâmetro
+            $cd_categoria = $param[0];
+
+            // Verificando se o código está preechido
+            if (!empty($cd_categoria)) {
+                // Chamando Validação
+                $catValidate = new Add();
+                $catValidate->validate();
+
+                // Pegando Dados da requisição de forma dinâmica e automática
+                $sanitized = new Sanitize();
+                $data = $sanitized->sanitized();
+
+                // Definindo Código
+                $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+
+                // Buscando Dados
+                $categoriaFuncionario = $this->CategoriaFuncionario->select();
+
+                // Formatando Status para formulário
+                if ($categoriaFuncionario->ic_status == 1) {
+                    $ic_status = 'enable';
+                } else {
+                    $ic_status = 'disable';
+                }
+
+                // Verificando erro de Validação
+                if (!$catValidate->hasErrors()) {
+                    $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+                    $this->CategoriaFuncionario->nm_categoria = $data->nm_categoria;
+                    $this->CategoriaFuncionario->setIcStatus($data->ic_status);
+                    $this->CategoriaFuncionario->nm_sigla = $data->nm_sigla;
+                    $this->CategoriaFuncionario->update();
+
+                    // Redirecionando para a action index
+                    $this->redirectUrl($this->controller);
+                    exit;
+                }
+
+                // Caregando Helpers
+                $bootstrapHelper = parent::loadHelper("Bootstrap");
+                $styleHelper = parent::loadHelper("Style");
+                $linkHelper = parent::loadHelper("Link");
+                $formHelper = parent::loadHelper("Form");
+
+                // Carregando View
+                require_once parent::loadView($this->controller, $this->currentAction);
+                exit;
+            }
+
+            $this->redirectUrl();
+            exit;
+        }
+
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
         exit;
     }
 
     public function view(array $param)
     {
-        // Pegando valor do parâmetro
-        $cd_categoria = $param[0];
+        // Instanciando a classe
+        $auth = new Auth();
 
-        // Verificando se o código está preechido
-        if (!empty($cd_categoria)) {
-            // Definindo Código
-            $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            // Pegando valor do parâmetro
+            $cd_categoria = $param[0];
 
-            // Buscando Dados
-            $categoriaFuncionario = $this->CategoriaFuncionario->select();
+            // Verificando se o código está preechido
+            if (!empty($cd_categoria)) {
+                // Definindo Código
+                $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
 
-            // Caregando Helpers
-            $bootstrapHelper = parent::loadHelper("Bootstrap");
-            $styleHelper = parent::loadHelper("Style");
-            $linkHelper = parent::loadHelper("Link");
+                // Buscando Dados
+                $categoriaFuncionario = $this->CategoriaFuncionario->select();
 
-            // Carregando View
-            require_once parent::loadView($this->controller, $this->currentAction);
+                // Caregando Helpers
+                $bootstrapHelper = parent::loadHelper("Bootstrap");
+                $styleHelper = parent::loadHelper("Style");
+                $linkHelper = parent::loadHelper("Link");
+
+                // Carregando View
+                require_once parent::loadView($this->controller, $this->currentAction);
+                exit;
+            }
+
+            $this->redirectUrl();
             exit;
         }
 
-        echo 'É necessário um código';
-        $this->redirectUrl();
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
         exit;
     }
 
     public function disable(array $param)
     {
-        // Pegando valor do parâmetro
-        $cd_categoria = $param[0];
+        // Instanciando a classe
+        $auth = new Auth();
 
-        // Verificando se o código está preechido
-        if (!empty($cd_categoria)) {
-            // Definindo Código
-            $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            // Pegando valor do parâmetro
+            $cd_categoria = $param[0];
 
-            // Desabilitando unidade
-            $this->CategoriaFuncionario->disable();
+            // Verificando se o código está preechido
+            if (!empty($cd_categoria)) {
+                // Definindo Código
+                $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+
+                // Desabilitando unidade
+                $this->CategoriaFuncionario->disable();
+                $this->redirectUrl();
+                exit;
+            }
+
             $this->redirectUrl();
             exit;
         }
 
-        echo 'É necessário um código';
-        $this->redirectUrl();
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
         exit;
     }
 
     public function enable(array $param)
     {
-        // Pegando valor do parâmetro
-        $cd_categoria = $param[0];
+        // Instanciando a classe
+        $auth = new Auth();
 
-        // Verificando se o código está preechido
-        if (!empty($cd_categoria)) {
-            // Definindo Código
-            $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            // Pegando valor do parâmetro
+            $cd_categoria = $param[0];
 
-            // Habilitando unidade
-            $this->CategoriaFuncionario->enable();
+            // Verificando se o código está preechido
+            if (!empty($cd_categoria)) {
+                // Definindo Código
+                $this->CategoriaFuncionario->cd_categoria = $cd_categoria;
+
+                // Habilitando unidade
+                $this->CategoriaFuncionario->enable();
+                $this->redirectUrl();
+                exit;
+            }
+
+            echo 'É necessário um código';
             $this->redirectUrl();
             exit;
         }
 
-        echo 'É necessário um código';
-        $this->redirectUrl();
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
         exit;
     }
 }
