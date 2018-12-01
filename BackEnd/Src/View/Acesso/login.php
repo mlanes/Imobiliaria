d3f17b316e558c1c02ad6cddbfdd92b1e6035aaa
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-12-01 15:18:46 -0200

    Adicionando novo layout de login

--- BackEnd/Src/View/Acesso/login.php
+++ BackEnd/Src/View/Acesso/login.php
@@ -1,7 +1,28 @@
-<form method="POST">
-    <label>Login</label><br>
-    <input type="text" name="login" value="<?= $login ?>"><br>
-    <label>Password</label><br>
-    <input type="password" name="password"><br>
-    <button>Testar</button>
-</form>
+<!DOCTYPE html>
+<html lang="<?= SYSTEM_LANG ?>">
+    <head>
+        <?php
+            require_once parent::loadView('Layout', 'head_admin');
+        ?>
+    </head>
+    <body>
+        <div class="bg-login">
+            <div class="modal-login painel">
+                <h4 class="title">Imobiliária Litoral</h4>
+                <form method="post">
+                    <?php
+                        echo $formHelper->control('nm_login', ['label' => [
+                            'text' => 'Login',
+                            'class' => 'font-weight-bold text-white',
+                        ], 'type' => 'text', 'class' => 'form-control', 'block' => true, 'required' => true, 'placeholder' => 'Digite seu Login ']);
+                        echo $formHelper->control('nm_password', ['label' => [
+                            'text' => 'Senha',
+                            'class' => 'font-weight-bold text-white',
+                        ], 'type' => 'password', 'class' => 'form-control', 'block' => true, 'required' => true, 'placeholder' => 'Digite sua senha']);
+                    ?>
+                    <button class="btn botaoLogin mt-3">Salvar</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
