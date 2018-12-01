f42e2465c20bca8a42a4b8431d3d8cc17cb7a2cd
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-12-01 15:23:37 -0200

    Alterações na lógica de login e validação dos dados.

--- BackEnd/Src/Controller/AcessoController.php
+++ BackEnd/Src/Controller/AcessoController.php
@@ -2,6 +2,8 @@
 
 use Core\Controller;
 use Validate\Acesso\Login as LoginValidate;
+use Validate\Sanitize;
+use Auth\Auth;
 
 class AcessoController extends Controller
 {
@@ -16,39 +18,69 @@ class AcessoController extends Controller
 
     public function index()
     {
-        $acessos = $this->Acesso->list();
-        $count = $this->Acesso->countItems();
-        $bootstrapHelper = parent::loadHelper("Bootstrap");
-        $styleHelper = parent::loadHelper("Style");
-        $linkHelper = parent::loadHelper("Link");
-        require_once parent::loadView($this->controller, $this->currentAction);
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if ($auth->verifyAuthenticated()) {
+            $acessos = $this->Acesso->list();
+            $count = $this->Acesso->countItems();
+            $bootstrapHelper = parent::loadHelper("Bootstrap");
+            $styleHelper = parent::loadHelper("Style");
+            $linkHelper = parent::loadHelper("Link");
+            require_once parent::loadView($this->controller, $this->currentAction);
+            exit;
+        }
+
+        // Redirecionando para o login
+        $this->redirectUrl('Acesso/Login');
+        exit;
     }
 
     public function login()
     {
-        $login = isset($_POST['login']) ? trim($_POST['login']) : null;
-        $password = isset($_POST['password']) ? trim($_POST['password']) : null;
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+            // Chamando Validação
+            $loginValidate = new LoginValidate();
+            $loginValidate->validate();
 
-        $loginValidate = new LoginValidate();
-        $loginValidate->validate();
+            if (!$loginValidate->hasErrors()) {
+                // Pegando Dados da requisição de forma dinâmica e automática
+                $sanitized = new Sanitize();
+                $data = $sanitized->sanitized();
 
-        if (!$loginValidate->hasErrors()) {
-            $this->Acesso->nm_login = $login;
-            $this->Acesso->nm_password = $password;
-            if ($this->Acesso->login()) {
-                $_SESSION['login'] = $login;
-                $this->redirectUrl();
-            } else {
-                echo 'Usuário ou senha inválidos.';
-                require_once parent::loadView($this->controller, $this->currentAction);
+                // Atribuindo dados
+                $this->Acesso->nm_login = $data->nm_login;
+                $this->Acesso->nm_password = $data->nm_password;
+
+                // Autenticando
+                if ($this->Acesso->login()) {
+                    // Gravando o nome do usuário na sessão
+                    $_SESSION['login'] = $data->nm_login;
+
+                    // Redirecionando
+                    $this->redirectUrl();
+                    exit;
+                }
             }
+
+            // Caregando Helpers
+            $bootstrapHelper = parent::loadHelper("Bootstrap");
+            $styleHelper = parent::loadHelper("Style");
+            $linkHelper = parent::loadHelper("Link");
+            $formHelper = parent::loadHelper("Form");
+
+            // Carregando View
+            require_once parent::loadView($this->controller, $this->currentAction);
             exit;
         }
-        if (empty($_SESSION)) {
-            require_once parent::loadView($this->controller, $this->currentAction);
-        } else {
-            $this->redirectUrl(' ');
-        }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
         exit;
     }
     
@@ -60,43 +92,91 @@ class AcessoController extends Controller
 
     public function add()
     {
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+        }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
+        exit;
     }
 
     public function edit(array $param)
     {
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+        }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
+        exit;
     }
 
     public function view(array $param)
     {
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+        }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
+        exit;
     }
 
     public function disable(array $param)
     {
-        $cd_acesso = $param[0];
-        if ($cd_acesso != "") {
-            $this->Acesso->cd_acesso = $cd_acesso;
-            $this->Acesso->disable();
-            $this->redirectUrl();
-            exit;
-        } else {
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+            $cd_acesso = $param[0];
+            if (!empty($cd_acesso)) {
+                $this->Acesso->cd_acesso = $cd_acesso;
+                $this->Acesso->disable();
+                $this->redirectUrl();
+                exit;
+            }
             echo 'É necessário um código';
             $this->redirectUrl();
             exit;
         }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
+        exit;
     }
 
     public function enable(array $param)
     {
-        $cd_acesso = $param[0];
-        if ($cd_acesso != "") {
-            $this->Acesso->cd_acesso = $cd_acesso;
-            $this->Acesso->enable();
-            $this->redirectUrl();
-            exit;
-        } else {
+        // Instanciando a classe
+        $auth = new Auth();
+        
+        // Verificando se o usuário está logado
+        if (!$auth->verifyAuthenticated()) {
+            $cd_acesso = $param[0];
+            if (!empty($cd_acesso)) {
+                $this->Acesso->cd_acesso = $cd_acesso;
+                $this->Acesso->enable();
+                $this->redirectUrl();
+                exit;
+            }
             echo 'É necessário um código';
             $this->redirectUrl();
             exit;
         }
+
+        // Redirecionando para o dashboard
+        $this->redirectUrl(' ');
+        exit;
     }
 }
